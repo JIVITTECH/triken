e738d00549a581126d50439d6cc88b8237db1532
Sara <saj@klrtech.in>
2021-10-20 20:45:54 +0530

    Create database.php

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+$dbname = "cms_main";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("connection failed:" . $conn->connect_error);
+}
+mysqli_set_charset($conn, "utf8");
+
+?>
