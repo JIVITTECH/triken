d3e5528750e1b51e82eb034b10deb786fc3026a9
Sara <saj@klrtech.in>
2021-11-05 21:37:03 +0530

    ORDER SUMMAY PAGE (PAYMENT SUCCESSFUL) -> Load the details dynamically

---
+++ api/order_summary.php
@@ -0,0 +1,211 @@
+<?php
+
+include "../database.php";
+$cart_id = $_GET['cus_cart_id']; // 1 - live order, 2 - Past Orders
+
+    $sql = "select
+        cd.*,cd.customer_id as cus_id ,
+		DATE_FORMAT(cd.ordered_date_time, '%d %b %Y, %r') AS ordered_date_time_formatted,
+        id.razor_pay_id   
+        from obo_cart_details cd
+        left join invoice_details id
+	    on id.invoice_id = cd.invoice_no                
+        where cd.cart_id = $cart_id
+        AND cd.order_placed = 'Y' AND invoice_no <> ''
+        order by cart_id desc";
+
+$res_sql = mysqli_query($conn, $sql);
+$res_sql_cnt = mysqli_num_rows($res_sql);
+
+$obo_cart_arr = array();
+
+if ($res_sql_cnt > 0) {
+
+    while ($rw = mysqli_fetch_array($res_sql)) {
+
+        $obo_cart_details = array();
+        $wera_order_no = $rw['temp_order_id'];
+        $cart_id = $rw['cart_id'];
+        $ordered_date_time = $rw['ordered_date_time_formatted'];
+        $tax = $rw['tax'];
+        $total_price = $rw['total_price'];
+        $invoice_no = $rw['invoice_no'];
+        $order_placed = $rw['order_placed'];
+        $branch_address = $rw['branch_address'];
+		$delivery_address  = $rw['delivery_address'];
+        $delivery = $rw['delivery'];
+        $del_charge = $rw['del_charge'];
+        $pkg_charge = $rw['packing_charge'];
+        $payment_type = $rw['razor_pay_id'];
+        $cart_detail_tmp = array(
+				"cart_details" => array(
+                "cart_id" => $cart_id,
+                "ordered_date_time" => $ordered_date_time,
+                "delivery_address" => $delivery_address,
+                "tax" => $tax,
+                "total_price" => $total_price,
+                "invoice_no" => $invoice_no,
+                "order_placed" => $order_placed,
+                "branch_address" => $branch_address,
+                "delivery" => $delivery,
+                "pkg_charge" => $pkg_charge,
+                "del_charge" => $del_charge,
+                "payment_type" => $payment_type,
+				"del_day" => "01/10/2021",
+                "payment_type" => "7-9 AM"
+            )
+        );
+
+        array_push($obo_cart_details, $cart_detail_tmp);
+
+        $sql_items = "select DISTINCT oboi.cart_item_id AS cart_item_id_1, oboi.*, 
+                        pm.name, pm.image,pm.tax, COALESCE(GROUP_CONCAT(sm.name), '') AS sub_mod_name,
+                        if(sum(sm.price)is null,0,sum(sm.price))  AS mod_price
+                        from obo_cart_item_details oboi
+                        left join predefined_menu pm
+                        on pm.predef_menu_id = oboi.predef_menu_id AND pm.branch = oboi.branch_id
+                        left join obo_cart_item_modifiers oboim
+                        on oboim.cart_item_id = oboi.cart_item_id
+                        left join sub_modifiers sm
+                        on sm.mod_sub_id = oboim.mod_sub_id
+                        where oboi.cart_id = $cart_id
+                        group by oboi.cart_item_id
+                        order by oboi.cart_item_id";
+
+        $res_sql_items = mysqli_query($conn, $sql_items);
+        $res_sql_items_cnt = mysqli_num_rows($res_sql_items);
+
+        $cart_items_cnt = array(
+            "cart_items_count" => "$res_sql_items_cnt"
+        );
+
+        array_push($obo_cart_details, $cart_items_cnt);
+
+        if ($res_sql_items_cnt > 0) {
+
+            $cart_item_tmp = array();
+            $cart_item_tmp2 = array();
+
+            $sub_total = 0.0;
+
+            while ($row = mysqli_fetch_array($res_sql_items)) {
+
+                $cart_item_id = $row['cart_item_id'];
+                $predef_menu_id = $row['predef_menu_id'];
+                $price = $row['price'];
+                $quantity = $row['quantity'];
+                $name = $row['name'];
+                $image = $row['image'];
+                $sub_mod_name = $row['sub_mod_name'];
+                $mod_price = $row['mod_price'];
+                $tax = $row['tax'];
+
+                $sub_total = $sub_total + $price + ($quantity * $mod_price) + ($price/ 100 * $tax);
+
+                $cart_item_tmp = array(
+                    "cart_item_id" => $cart_item_id,
+                    "cart_id" => $cart_id,
+                    "predef_menu_id" => $predef_menu_id,
+                    "price" => $price + ($quantity * $mod_price) + ($price/ 100 * $tax),
+                    "quantity" => $quantity,
+                    "name" => $name,
+                    "image_path" => $image,
+                    "sub_mod_name" => $sub_mod_name,
+                 );
+
+                $cart_item_tmp2[] = $cart_item_tmp;
+            }
+
+            $cart_items = array(
+                "cart_items" => $cart_item_tmp2,
+            );
+
+            $cart_sub_total = array(
+                "cart_sub_total" => $sub_total
+            );
+
+            array_push($obo_cart_details, $cart_items);
+            array_push($obo_cart_details, $cart_sub_total);
+        }
+
+        $sel_tax = "select * from obo_order_tax where cart_id = $cart_id";
+        $res_sel_tax = mysqli_query($conn, $sel_tax);
+        $res_sel_tax_cnt = mysqli_num_rows($res_sel_tax);
+
+        if ($res_sel_tax_cnt > 0) {
+
+            while ($row1 = mysqli_fetch_array($res_sel_tax)) {
+
+                $cart_id = $row1['cart_id'];
+                $tax_name = $row1['tax_name'];
+                $tax_per = $row1['tax_per'];
+                $tax_cost = $row1['tax_cost'];
+
+                $cart_tax_tmp = array(
+                    "cart_id" => $cart_id,
+                    "tax_name" => $tax_name,
+                    "tax_per" => $tax_per,
+                    "tax_cost" => $tax_cost
+                );
+
+                $cart_tax_tmp2[] = $cart_tax_tmp;
+            }
+
+            $cart_tax = array(
+                "cart_taxes" => $cart_tax_tmp2
+            );
+
+            array_push($obo_cart_details, $cart_tax);
+        } else {
+            $cart_tax = array(
+                "cart_taxes" => array()
+            );
+
+            array_push($obo_cart_details, $cart_tax);
+        }
+
+        $sel_disc = "select * from obo_cart_coupon where cart_id = $cart_id";
+        $res_sel_disc = mysqli_query($conn, $sel_disc);
+        $res_sel_disc_cnt = mysqli_num_rows($res_sel_disc);
+
+        if ($res_sel_disc_cnt > 0) {
+
+            while ($row2 = mysqli_fetch_array($res_sel_disc)) {
+
+                $cart_id = $row2['cart_id'];
+                $disc_name = $row2['discount_name'];
+                $disc_cost = $row2['discount_cost'];
+
+                $cart_disc_tmp = array(
+                    "cart_id" => $cart_id,
+                    "disc_name" => $disc_name,
+                    "disc_cost" => $disc_cost
+                );
+
+                $cart_disc_tmp2[] = $cart_disc_tmp;
+            }
+
+            $cart_disc = array(
+                "cart_disc" => $cart_disc_tmp2
+            );
+
+            array_push($obo_cart_details, $cart_disc);
+        } else {
+            $cart_disc = array(
+                "cart_disc" => array()
+            );
+
+            array_push($obo_cart_details, $cart_disc);
+        }
+        array_push($obo_cart_arr, $obo_cart_details);
+    }
+
+
+    $db = json_encode($obo_cart_arr);
+    echo json_encode($obo_cart_arr);
+
+    //print_r($obo_cart_arr);
+    //$db_arr = json_decode($db);
+    //print_r($db_arr);
+}
+?>
---
+++ order_summary.php
@@ -0,0 +1,168 @@
+<?php
+    $title = 'Thank you- Triken ';
+    $description = 'The only thing we stock is the packages we use to deliver the meat.';
+    $pageRobots = 'noindex,nofollow';
+    $image = ' '; 
+    $pageCanonical = '';
+    $url = ' '; 
+    $page ="Deal of the Day";
+    include('header.php');
+    include('main.php');
+?> 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+
+<section class="thankyou">
+    <div class="container">
+        <div class="order-success ttext">
+            <ul>
+                <li><img src="assets/images/tick.svg">  </li>
+                <li><h3> Thank you for your order! :)</h3>
+            <p>We’ve received your order.</p></li>
+            </ul>
+            <table class="orderlist">
+                <tr>
+                    <td><p>Order Number:</p> <h6> #<span id ="generated_cart_id"> </span></h6></td>
+                    <td><p>Total:</p> <h6> ₹<span id ="grand_total"></span> </h6></td>
+                </tr>
+                <tr>
+                    <td><p>Date:</p> <h6><span id ="order_date"> </span> </h6></td>
+                    <td><p>Payment Method:</p> <h6> <span id ="payment_mode"></span></h6></td>
+                </tr>
+            </table>
+        </div>
+                    <div class="order-details-wrapper mb-5">
+                        <h4>Order Details</h4>
+                        <table class="order-table">
+                            <thead>
+                                <tr>
+                                    <th>Product</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody id="item_tag">
+                              
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td>Subtotal:</td>
+                                    <td>₹<span id="sub_total"></span></td>
+                                </tr>
+                                <tr>
+                                    <td>Delivery:</td>
+                                    <td>₹<span id="del_charge"></span></td>
+                                </tr>
+                                <tr>
+                                    <td>Payment method:</td>
+                                    <td><span id="mode"></span></td>
+                                </tr>
+                                <tr>
+                                    <td>Discount:</td>
+                                    <td>₹<span id="discount"></span></td>
+                                </tr>
+                                <tr class="total">
+                                    <td class="border-no">Total:</th>
+                                    <td class="border-no">₹<span id="g_total"></td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <!-- End of Order Details -->
+
+                    <div id="account-addresses">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="ecommerce-address shipping-address">
+                                    <h4>Shipping Address</h4>
+                                        <table class="address-table">
+                                            <tbody>
+                                                <tr>
+                                                    <td>
+													<address id="del_address"></address></td>
+                                                </tr>
+                                                <tr>
+                                                    <td><span class="dslot">Delivery Slot </span>
+                                                    <span class="slotdt"id="del_slot"></span></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End of Account Address -->
+
+                    <a href="#" class="btn btn-dark "><i class="w-icon-long-arrow-left"></i>Back to Home</a>
+        </div>
+    </div>
+</section>
+<script>
+ $(document).ready(function () {
+	loadOrderDetails();
+});
+
+	function loadOrderDetails() {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function () {
+		if (this.readyState === 4 && this.status === 200) {
+			var myObj = JSON.parse(this.responseText);
+				if (myObj.length > 0) {
+					var temp_cart_id = [];   
+					var temp_cart_id1 = [];
+					for (var i = 0; i < myObj.length; i++) {
+
+					var cart_arr = myObj[i];
+					var cart_details = cart_arr[0].cart_details;
+					var cart_items_count = cart_arr[1].cart_items_count;
+					var cart_items = cart_arr[2].cart_items;
+					var cart_sub_total = cart_arr[3].cart_sub_total;
+					var cart_taxes = cart_arr[4].cart_taxes;
+					var cart_disc = cart_arr[5].cart_disc;
+					var order_type = cart_details.delivery;
+					var cart_payment_mode = "";
+					
+					if(cart_details.payment_type !== "" ){
+						cart_payment_mode = "Bank Transfer";
+					}else{
+						cart_payment_mode = "Cash on Delivery";
+					}
+					
+					var del_slot = cart_details.del_day + " | " + cart_details.del_day + " slot";
+					document.getElementById("generated_cart_id").innerHTML =  cart_details.cart_id;
+					document.getElementById("order_date").innerHTML =  cart_details.ordered_date_time;
+					document.getElementById("grand_total").innerHTML =  cart_details.total_price;
+					document.getElementById("payment_mode").innerHTML =  cart_payment_mode;
+					document.getElementById("del_address").innerHTML =  cart_details.delivery_address;
+					document.getElementById("sub_total").innerHTML = cart_sub_total.toFixed(2) ;
+					document.getElementById("g_total").innerHTML =  cart_details.total_price;
+					document.getElementById("mode").innerHTML =  cart_payment_mode;
+					document.getElementById("del_slot").innerHTML =   del_slot;
+					document.getElementById("del_charge").innerHTML =  cart_details.del_charge;
+					if (cart_disc.length > 0) {
+						for (var l = 0; l < cart_disc.length; l++) {
+							document.getElementById("discount").innerHTML =  cart_disc[l].disc_cost;
+						}
+					}
+					
+					if (cart_items.length > 0) {
+
+						for (var k = 0; k < cart_items.length; k++) {
+							  
+							$('#item_tag').append('<tr>' +
+														'<td>' +
+															'<a href="#">' + cart_items[k].name + '</a>&nbsp;<strong>x ' + cart_items[k].quantity + '</strong>' +
+														'</td>'+
+														'<td> ₹ ' + cart_items[k].price.toFixed(2) + ' </td>' +
+													'</tr>');
+								}
+							}
+						}
+					}
+				}
+			};
+	var url = "api/order_summary.php?cus_cart_id=404";
+	xhttp.open("GET", url, true);
+	xhttp.send();
+	}
+</script>
+
+<?php include('footer.php'); ?>
