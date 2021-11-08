df88d9e4d546f951581c61ffc98b2f0a6aa950d6
jaf <jaf@klrtech.in>
2021-11-08 22:07:28 +0530

    get profile details api call

---
+++ api/get_profile_details.php
@@ -0,0 +1,35 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
+include("../database.php");
+
+$user_id = $_SESSION['user_id'];
+
+$query = "select customer_name, contact_no, email_addr from kot_customer_details where id = $user_id;
+
+$result = mysqli_query($conn, $query);
+
+$res = '';
+
+while ($rows = mysqli_fetch_array($result)) {
+
+    $events = array(
+        "id" => "$rows[id]",
+        "customer_name" => "$rows[customer_name]",
+        "contact_no" => "$rows[contact_no]",
+        "email_addr" => "$rows[email_addr]"
+    );
+
+    $output[] = $events;
+    $res = json_encode($output);
+
+}
+
+if ($res == "") {
+    $res = json_encode([]);
+}
+
+echo $res;
+
+?>
