d5be4c505c914f852816d1df7f44110bd587be99
Anand <adu@klrtech.in>
2021-11-12 07:38:33 +0530

    ADU-TRIKEN-1044

--- menu.php
+++ menu.php
@@ -15,7 +15,7 @@
                             <form method="get" action="#"
                                 class="input-wrapper header-search hs-expanded hs-round d-md-flex">
                                 <div class="select-box">
-                                    <a class="button" href="#popup1"><i class="fa fa-map-marker" aria-hidden="true"></i>
+                                    <a class="button" href=""><i class="fa fa-map-marker" aria-hidden="true"></i>
      VOC Colony, Peelamedu</a>
                                 </div>
                                 <button class="btn btn-search productsearch hidden-sm" type="submit">
