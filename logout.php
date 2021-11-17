17b4ab9d5af3f2bced238d64cdb76b35c7a6b25e
Anand <adu@klrtech.in>
2021-11-17 23:00:47 +0530

    ADU-TRIKEN-1123

--- logout.php
+++ logout.php
@@ -11,20 +11,20 @@ unset($_SESSION['user_loc_latitude']);
 
 if (isset($_COOKIE['item_list'])) {
     unset($_COOKIE['item_list']);
-    setcookie('item_list', '', time() - 3600, '/'); // empty value and old timestamp
+    setcookie('item_list', '', time()+(3600*24*30), '/'); // empty value and old timestamp
 }
 
 if (isset($_COOKIE['user_id'])) {
-    setcookie("user_id", "", time()-3600);
+    setcookie("user_id", "", time()+(3600*24*30));
 }
 if (isset($_COOKIE['mobile_no'])) {
-    setcookie("mobile_no", "", time()-3600);
+    setcookie("mobile_no", "", time()+(3600*24*30));
 }
 if (isset($_COOKIE['branch_id'])) {
     setcookie("branch_id", "-1", time()+(3600*24*30));
 }
 if (isset($_COOKIE['cart_id'])) {
-    setcookie("cart_id", "", time()-3600);
+    setcookie("cart_id", "", time()+(3600*24*30));
 }
 
 ?>
