ec0e3124cb98e476fe803b6ffca0dd3e62872aa2
Anand <adu@klrtech.in>
2021-11-24 00:28:28 +0530

    ADU-TRIKEN-1092

--- js/index.js
+++ js/index.js
@@ -147,7 +147,34 @@ function loadLtdDealsOfTheDay() {
 	xmlhttp.onreadystatechange = function () {
 		if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
 			var myObj = JSON.parse(this.responseText);
+			information = information + '<div class="container mb-4">' +
+			                                '<div class="widget-body br-sm h-100">' +
+											     '<div class="row">' + 
+												      '<div class="col-lg-8 col-sm-12"> <h1 class="title text-left appear-animate"> <img src="assets/images/certificate.svg"> Deal of the Day  </h1> </div>' +
+													  '<div class="col-lg-4 col-sm-12">' +
+													      '<div class="viewall">' +
+														       '<a href="dod.php" class="btn btn-dark"> View All </a>' +
+														  '</div>' +
+													  '</div>' +
+												 '</div>' +
+											'<div class="swiper slider_sec">';
 			if (myObj.length !== 0) {
+			    information = information + "<div class=" + '"swiper-container swiper-theme nav-top"' + ' data-swiper-options="{' +
+				                                            "'slidesPerView': 1.1," + 
+															"'spaceBetween': 10," +
+															"'breakpoints': {" +
+															    "'576': {" + 
+																   "'slidesPerView': 2" + 
+																"}," + 
+																"'768': {" + 
+																   "'slidesPerView': 3" + 
+                                                                "}," +
+																"'992': {" +
+																   "'slidesPerView': 4" + 
+                                                                "}" +
+															"}" +
+														'}">' +
+											"<div class='swiper-wrapper row cols-lg-1 cols-md-3'>";
                 for (var i = 0; i < myObj.length; i++) {
 					var cover_photo = myObj[i].image;
 					var image_path = "";
@@ -202,10 +229,15 @@ function loadLtdDealsOfTheDay() {
 													"</div>" +
 												"</div>";
 				}
-                $('#ltd_deals_of_the_day_container').append(information);
+				information = information + "</div>" + 
+				                            "<button class='swiper-button-next'></button>" +
+											"<button class='swiper-button-prev'></button>" +
+											"</div>";
             } else {
-		$('#ltd_deals_of_the_day_container').append("<center>No Items found</center>");
-            }
+			    information = information + "<center>No Items found</center>";
+	        }
+			information = information + '</div>' + '</div>' + '</div>';
+			 $('#ltd_deals_of_the_day_container').append(information);
 		}
 	};
 }
