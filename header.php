0aa6358d9517ac9e7b2e85ecbf2b08a0681007be
Sara <saj@klrtech.in>
2021-11-29 17:30:00 +0530

    geo location issue was fixed:

--- header.php
+++ header.php
@@ -211,9 +211,28 @@
                 };
             }
 			
+			$(document).ready(function () {
+				var loadDynamicData = "";
+				$.get("api/load_home_page.php?action=get_list_of_cities", function (data, status) {
+					var jsonStr = JSON.parse(data);
+					for (var i = 0; i < jsonStr.length; i++) {
+						if (jsonStr[i].branch_exists === 'Y') {
+							loadDynamicData = loadDynamicData+ "<div><label for='chkYes' class='location'><input type='radio' id='chkYes' class='loc' name='chkPassPort' onclick='ShowHideDiv()'/>" + jsonStr[i].city_name + "</label> <br><span class='soon hide'> Coming Soon!</span></div>";
+						} else {
+							loadDynamicData = loadDynamicData+"<div><label for='chkNo' class='no location'><input type='radio' id='chkNo' class='loc' name='chkPassPort' onclick='ShowHideDiv()'/> " + jsonStr[i].city_name + "</label> <br><span class='soon'> Coming Soon!</span></div>";
+						}
+					}
+					$('#cities').append(loadDynamicData);
+				});
+
+			});
 	</script>
 		
 </head>
+<div class='lpopup popup'>
+	<?php include('location.php'); ?>
+</div>  
+
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
 
 
