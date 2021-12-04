795e4fc6354398e35b24b92dd5ab9cb0548c265e
Sara <saj@klrtech.in>
2021-12-04 15:37:50 +0530

    Code was added

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="facebook-domain-verification" content="q9czenccdpgsj8e53cbhyk34pp40xt" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <script src="js/basics.js"></script>
 	<title>
