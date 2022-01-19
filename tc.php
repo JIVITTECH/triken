dbf3906e382ed7d147666f7efb063d8edf6b938e
Sara <saj@klrtech.in>
2022-01-19 21:17:51 +0530

    Few issue was fixed

--- tc.php
+++ tc.php
@@ -22,20 +22,20 @@ $page ="Terms & Conditions";
 								<p class=" mx-auto text-center">Welcome to Triken’s and our Terms of conditions. These Terms are important
                                                                     and affect your legal rights, so please read them carefully.<p class="mx-auto text-center">
 								
-                                                                <h3  class="title title-center" >AGE LIMITATION</h3>    
+                                                                <h3  class="title title-center" style="font-size:20px !important;" >AGE LIMITATION</h3>    
                                                                 <p class="mx-auto text-center">If you are under 18, you may use the Websites only under the supervision of a
 parent or guardian.
                                                                 </p>
-<h3 class="title title-center">USER CONDUCT</h3>    
+<h3 class="title title-center" style="font-size:20px !important;">USER CONDUCT</h3>    
                                                                 <p class="mx-auto">You agree that you will not violate, or encourage others to violate, any law,
 contract, intellectual property or other third party right, or commit a tort, and that
 you are solely responsible for your conduct, while accessing or using the Sites.
 You agree that you will abide by these Terms and will not: </p>
 
  <p class="mx-auto">i) Engage in any harassing, threatening, intimidating, predatory or stalking
-conduct; </p>
+conduct </p>
  <p class="mx-auto">ii) Use or attempt to use another user’s Account without authorization from such
-user; </p>
+user </p>
  <p class="mx-auto">iii) Use the Sites in any manner that could interfere with, disrupt, negatively affect
 or inhibit other users from fully enjoying the Sites or that could damage, disable,
 overburden or impair the functioning of the Sites in any manner, including by (a)
@@ -43,29 +43,29 @@ uploading or otherwise disseminating any virus, adware, spyware, worm, or other
 malicious code, (b) making any unsolicited offer or advertisement to another user
 of the Sites, (c) collecting personal information about another user or third party
 without consent, or (d) interfering with or disrupting any network, equipment, or
-server connected to or used to provide the Sites;
+server connected to or used to provide the Sites
  </p>
   <p class="mx-auto">iv) Reverse engineer any aspect of the Sites or do anything that might discover
 source code or bypass or circumvent measures employed to prevent or limit
-access to any Content, area or code of the Sites; </p>
+access to any Content, area or code of the Sites </p>
  <p class="mx-auto">v) Attempt to circumvent any content-filtering techniques we employ or attempt
 to access any feature or area of the Sites that you are not authorized to access; </p>
  <p class="mx-auto">vi) Develop any third-party applications that interact with User Content or the
-Sites without our prior written consent; </p>
+Sites without our prior written consent</p>
  <p class="mx-auto">vii) Use any robot, spider, crawler, scraper, script, browser extension, offline
 reader or other automated means or interface not authorized by us to access the
 Sites, extract data or otherwise interfere with or modify the rendering of Site
-pages or functionality; </p>
+pages or functionality </p>
  <p class="mx-auto">vii) Use the Sites for any illegal or unauthorized purpose, or engage in, encourage
 or promote any activity that violates these Terms. </p>
  
- <h3  class="title title-center" >NO RESALE</h3>
+ <h3  class="title title-center" style="font-size:20px !important;">NO RESALE</h3>
 <p class="mx-auto text-center">You are not permitted to resell or otherwise use the Products for commercial
 purposes.</p>
 
-<h3  class="title title-center" >RETURNS</h3>
+<h3  class="title title-center"style="font-size:20px !important;" >RETURNS</h3>
 <p class="mx-auto text-center">If you are dissatisfied with your delivery for any reason like</p>
-<p class="mx-auto text-center">i) Altered or damaged,</p>
+<p class="mx-auto text-center">i) Altered or damaged</p>
 <p class="mx-auto text-center">ii) Defective or different item delivered to you</p>
 <p class="mx-auto text-center">Please contact us Ph No 8686 754 754 or email at care@triken.in
 Then from our TRIKEN team a quality inspector will inspect the same and he
@@ -73,7 +73,7 @@ will decide whether to replace or not and also you will be required to share the
 proof of the same before quality inspector. No replacement / refund / or any
 other resolution will be provided without quality inspector.</p>
 
-<h3  class="title title-center" >MODIFICATIONS TO THE SERVICE AND PRICES</h3>
+<h3  class="title title-center" style="font-size:20px !important;">MODIFICATIONS TO THE SERVICE AND PRICES</h3>
 <p class="mx-auto text-center">Prices for our products are subject to change without notice.</p>
 <p class="mx-auto text-center">We reserve the right at any time to modify or discontinue the Service (or any part
 or content thereof) without notice at any time.</p>
@@ -81,7 +81,7 @@ or content thereof) without notice at any time.</p>
 <p class="mx-auto text-center">We shall not be liable to you or to any third-party for any modification, price
 change, suspension or discontinuance of the Service.</p>
 
-<h3  class="title title-center" >ACCURACY OF BILLING AND ACCOUNT INFORMATION</h3>
+<h3  class="title title-center" style="font-size:20px !important;" >ACCURACY OF BILLING AND ACCOUNT INFORMATION</h3>
 <p class="mx-auto text-justify">We reserve the right to refuse any order you place with us. We may, in our sole
 discretion, limit or cancel quantities purchased per person, per household or per
 order. These restrictions may include orders placed by or under the same
@@ -97,7 +97,7 @@ your account and other information, including your email address and credit card
 numbers and expiration dates, so that we can complete your transactions and
 contact you as needed.</p>
 
-<h3  class="title title-center" >CHANGES TO TERMS OF SERVICE</h3>
+<h3  class="title title-center" style="font-size:20px !important;">CHANGES TO TERMS OF SERVICE</h3>
 <p class="mx-auto text-center">You can review the most current version of the Terms of Service at any time at
 this page.</p>
 <p class="mx-auto text-center">We reserve the right, at our sole discretion, to update, change or replace any part
@@ -106,7 +106,7 @@ your responsibility to check our website periodically for changes. Your continue
 use of or access to our website or the Service following the posting of any changes
 to these Terms of Service constitutes acceptance of those changes.</p>
 
-<h3  class="title title-center" >DISPUTES AND ARBITRATION</h3>
+<h3  class="title title-center" style="font-size:20px !important;" >DISPUTES AND ARBITRATION</h3>
 <p class="mx-auto text-center">We hope to make you a happy and satisfied customer, but if there is a dispute that
 needs to be resolved, outlines what is expected of both of us.</p>
 <p class="mx-auto text-justify">Before you may pursue or participate in a dispute (or raise such dispute as a
